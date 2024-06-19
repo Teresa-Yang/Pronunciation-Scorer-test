46c1cbe562c79fcf0b1c76881641da541f165e5f
Teresa Yang <yangte@sheridancollege.ca>
2024-06-19 11:19:42 -0400

    configured main file for deployment

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import traceback
 from FlaskServer import web
 import sys
 
-host, port, debug = (0, 0, 0)
-try:
-    host, port, debug = sys.argv[1:4]
-except:
-    print("Incorrect number of args given.")
-    print("Syntax: python main.py <host ip> <port> <debug (y/n)>")
-    exit(0)
+# host, port, debug = (0, 0, 0)
+# try:
+#     host, port, debug = sys.argv[1:4]
+# except:
+#     print("Incorrect number of args given.")
+#     print("Syntax: python main.py <host ip> <port> <debug (y/n)>")
+#     exit(0)
 
 try:
-    web.run(host, port, (debug == 'y' or debug == '1'))
     # host= "0.0.0.0"
     # port= 5000
-    # web.run(host, port)
+    # web.run(host, port, (debug == 'y' or debug == '1'))
+    
+    web.run(host="0.0.0.0", port=5000, debug=True)
 except:
     traceback.print_exc()
